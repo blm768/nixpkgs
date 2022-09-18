1edf4656855cd927d0eb8c06ee161aa69f4e98f3
Ben Merritt <blm768@gmail.com>
2022-09-18 13:54:40 -0700

    tiled: 1.8.4 -> 1.9.2

--- pkgs/applications/editors/tiled/default.nix
+++ pkgs/applications/editors/tiled/default.nix
@@ -1,19 +1,32 @@
-{ lib, mkDerivation, fetchFromGitHub, pkg-config, qmake
-, python3, qtbase, qttools }:
+{ lib, mkDerivation, fetchFromGitHub, pkg-config
+, python3, qbs, qttools, wrapQtAppsHook
+, qtbase, qtdeclarative, qtsvg }:
 
+let
+  qtModules = [ qtbase qtdeclarative qtsvg ];
+  qtLibPaths = map (drv: "${drv}/lib/pkconfig") qtModules;
+in
 mkDerivation rec {
   pname = "tiled";
-  version = "1.8.4";
+  version = "1.9.2";
 
   src = fetchFromGitHub {
     owner = "bjorn";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-QYA2krbwH807BkzVST+/+sjSR6So/aGY/YenEjYxE48=";
+    sha256 = "sha256-026OO7r8n1BUapUtKRHvqKdSZiClTQIiYfajiC2TAcQ=";
   };
 
-  nativeBuildInputs = [ pkg-config qmake ];
-  buildInputs = [ python3 qtbase qttools ];
+  nativeBuildInputs = [ pkg-config qbs wrapQtAppsHook ];
+  buildInputs = [
+    python3
+    qttools
+  ] ++ qtModules;
+
+  buildPhase = ''
+  export PYTHONHOME=${python3}
+  qbs projects.Tiled.qbsSearchPaths:'${builtins.toJSON qtLibPaths}'
+  '';
 
   meta = with lib; {
     description = "Free, easy to use and flexible tile map editor";
